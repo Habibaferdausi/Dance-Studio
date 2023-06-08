f0793e1a18593e8e81bbed19572b3a4c728adba2
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-06-09 01:48:58 +0600

    inst pageupdate

--- src/Components/Pages/InstructorsPage.jsx
+++ src/Components/Pages/InstructorsPage.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import useAxios from "../Hooks/useAxios";
 import { Card, Progress } from "flowbite-react";
+import {
+  FaFacebook,
+  FaPhone,
+  FaTwitter,
+  FaWhatsapp,
+  FaWhatsappSquare,
+} from "react-icons/fa";
 
 const InstructorsPage = () => {
   const [instructors, setInstructors] = useState([]);
@@ -27,25 +34,29 @@ const InstructorsPage = () => {
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Instructors</h1>
       {instructors.length > 0 ? (
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-9 mx-12">
           {instructors.map((instructor) => (
-            <Card key={instructor._id} className="mb-4">
-              <div className="flex flex-col items-center pb-10">
-                <div
-                  Display
-                  Name
+            <Card key={instructor._id} className="shadow-2xl">
+              <div className="flex flex-col items-center pb-3">
+                <img
                   alt="image"
                   className="mb-3 rounded-full shadow-lg"
-                  height="96"
-                  src={instructor.img}
+                  height="40"
+                  src={instructor?.photoURL}
                   width="96"
                 />
-                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                <h5 className="mb-1  font-bold text-2xl text-gray-700 dark:text-white">
                   {instructor?.name}
                 </h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">
+                <span className=" text-base text-gray-500 dark:text-gray-400">
                   {instructor?.email}
                 </span>
+                <div className=" flex gap-8 mt-5 text-4xl">
+                  {" "}
+                  <FaFacebook className="text-blue-500" />
+                  <FaWhatsapp className="text-green-500" />
+                  <FaTwitter className="text-blue-400" />
+                </div>
               </div>
             </Card>
           ))}
