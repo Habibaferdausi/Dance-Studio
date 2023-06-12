30894736cee185013a6773eb958673623f610a98
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-06-12 22:47:43 +0600

    add redme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Dance Studio
+
+Dance Studio is a Full Stack website. User can see the classes, instructors , many information about website. Here is Admin panel also. In Admin panel the Admin choose the instructor . And instructor can Upload the classes.. Then Approved by Admin This class is visible For all.
+
+## Features
+
+- See Classes And Instructor
+- Admin, Student , Instructor Dashboard
+- Add Classes By Student
+- Real Card Payment System
+- Instructor can Upload Classes
+- Admin can Approve or deny Class.
+
+## 🔗 Link
+
+[Dance Studio](https://dance-studio1.web.app)
--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -1,63 +1,50 @@
 import React from "react";
-import { Footer } from "flowbite-react";
-import {
-  FaDribbble,
-  FaFacebook,
-  FaGithub,
-  FaInstagram,
-  FaTwitter,
-} from "react-icons/fa";
 
 const FooterSection = () => {
   return (
-    <Footer container>
-      <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div>
-            <Footer.Brand
-              alt="Logo"
-              href="https://flowbite.com"
-              name="Dance Studio"
-              src="https://i.ibb.co/PwPjsVY/AK-removebg-preview.png"
+    <footer className="footer p-10 bg-base-200 text-base-content">
+      <div>
+        <img
+          src="https://i.ibb.co/PwPjsVY/AK-removebg-preview.png"
+          className="h-40"
+        ></img>
+        <h1 className="text-xl text-bold text-center">Dance Studio</h1>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </div>
+      <div>
+        <span className="footer-title">Contact Us</span>
+        <p>102/street NYC</p>
+        <p>Mobile: +12346789</p>
+        <span className="footer-title mt-5">Newsletter</span>
+        <div className="form-control w-80">
+          <label className="label">
+            <span className="label-text">Enter your email address</span>
+          </label>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="username@site.com"
+              className="input input-bordered w-full pr-16"
             />
-          </div>
-          <div className="grid grid-cols-3 gap-8 mt-4 ;g:mt-1 lg:grid-cols-4 sm:gap-6">
-            <div>
-              <Footer.Title title="about" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#"></Footer.Link>
-                <Footer.Link href="#"></Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Follow us" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Github</Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Legal" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms & Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-          </div>
-        </div>
-        <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright by="Habiba Ferdausi Ritu™" href="#" year={2023} />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <Footer.Icon href="#" icon={FaFacebook} />
-            <Footer.Icon href="#" icon={FaInstagram} />
-            <Footer.Icon href="#" icon={FaTwitter} />
-            <Footer.Icon href="#" icon={FaGithub} />
-            <Footer.Icon href="#" icon={FaDribbble} />
+            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
-    </Footer>
+    </footer>
   );
 };
 
