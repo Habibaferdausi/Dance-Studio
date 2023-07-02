ec7a66f223f0dc8b9bf63fbb4e24bf933433a559
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-07-02 13:29:05 +0600

    event section update

--- src/Components/Home/UpcomingEvents.jsx
+++ src/Components/Home/UpcomingEvents.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+// Helper function to get the month from a given date string
+const getMonth = (dateString) => {
+  const date = new Date(dateString);
+  const month = date.toLocaleString("default", { month: "long" });
+  return month;
+};
+
+// Helper function to get the year from a given date string
+const getYear = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  return year;
+};
+
 const UpcomingEvents = () => {
   const events = [
     {
@@ -16,7 +30,7 @@ const UpcomingEvents = () => {
     },
     {
       title: "Hip-Hop Masterclass",
-      date: "September 10, 2023",
+      date: "August  20, 2023",
       description:
         "Join our renowned guest instructor for an exclusive Hip-Hop Masterclass. This intensive workshop is designed for intermediate to advanced dancers looking to refine their hip-hop skills and learn the latest choreography trends.",
     },
@@ -33,7 +47,7 @@ const UpcomingEvents = () => {
       className="py-12 bg-fixed"
       style={{
         backgroundImage:
-          "url('https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')",
+          "url('https://images.unsplash.com/photo-1473691955023-da1c49c95c78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
@@ -51,7 +65,7 @@ const UpcomingEvents = () => {
           </span>
         </h1>
         <div
-          className="flex flex-col-reverse  items-end"
+          className="flex flex-col-reverse items-end"
           data-aos="fade-left"
           data-aos-offset="400"
           data-aos-easing="ease-in-sine"
@@ -62,10 +76,15 @@ const UpcomingEvents = () => {
               className="grid grid-cols-4 w-1/2 mb-4 bg-gradient-to-r from-gray-900 to-transparent opacity-75"
             >
               <div className="col-span-1 bg-blue-600 dark:bg-gray-900 p-6 flex flex-col items-center">
-                <p className="text-white text-4xl font-bold">
-                  {getMonth(event.date)}
+                <p
+                  className="text-white text-4xl font-bold"
+                  style={{ margin: "auto" }}
+                >
+                  {new Date(event.date).getDate()}
                 </p>
-                <p className="text-white text-2xl">{getYear(event.date)}</p>
+                <p className="text-white text-lg">
+                  {getMonth(event.date)} {getYear(event.date)}
+                </p>
               </div>
               <div className="col-span-3 bg-white dark:bg-gray-700 shadow-md p-6">
                 <h3 className="text-xl font-semibold text-gray-800 mb-2">
